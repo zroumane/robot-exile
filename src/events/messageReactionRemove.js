5babb4c6f48686e378c234a0293e8cbd633618c7
zroumane <zroumane@github.com>
2021-10-02 23:09:23 +0200

    up: event

--- src/events/messageReactionRemove.js
+++ src/events/messageReactionRemove.js
@@ -2,19 +2,9 @@ const { db } = require("../index.js");
 const setEvent = require("../utils/setEvent.js");
 
 module.exports = async (reaction, user) => {
-  const emoji = reaction.emoji.name;
-  const id = reaction.message.id;
-  const index = db.getIndex("/event", id, "id");
+  const index = db.getIndex("/event", reaction.message.id, "id");
   if (index == "-1") return;
   const msgEvent = await reaction.message.fetch();
   const event = db.getData(`/event[${index}]`);
-  event.choices.forEach(async (c, i) => {
-    if (c.emoji == emoji) {
-      db.push(
-        `/event[${index}]/choices[${i}]/members`,
-        c.members.filter((m) => m != user.id)
-      );
-    }
-  });
   setEvent(event, msgEvent);
 };
--- src/utils/setEvent.js
+++ src/utils/setEvent.js
@@ -1,4 +1,5 @@
 const { Message, MessageEmbed } = require("discord.js");
+const { client } = require("../index.js");
 const moment = require("moment");
 moment.locale("fr");
 
@@ -6,18 +7,33 @@ moment.locale("fr");
  * @param {Object} event
  * @param {Message} msg
  */
-module.exports = (event, msg) => {
+module.exports = async (event, msg) => {
   const embed = msg.embeds[0] ?? new MessageEmbed();
   embed.setTitle(`Event : ${event.name}`);
   const date = moment(event.date);
   embed.setDescription(`Date : ${date.format("dddd Do MMMM YYYY, H:mm")}, ${moment().to(date)}.`);
-  const totalMember = event.choices.reduce((total, current) => total + current.members.length, 0);
-  embed.fields = event.choices.map((c) => {
-    const name = `${c.emoji} ${c.name} (${c.members.length} - ${
-      c.members.length == 0 ? "0" : Math.round((c.members.length * 1000) / totalMember) / 10
-    }%)`;
-    const value = `> ${c.members.length == 0 ? "-" : c.members.map((m) => `<@${m}>`).join(", ")}`;
-    return { name, value };
-  });
-  return msg.edit({ embeds: [embed] });
+  const users = [];
+  for (const c of event.choices) {
+    const reaction = await msg.reactions.resolve(c.emoji);
+    if (!reaction || !reaction.me) {
+      await msg.react(c.emoji);
+      users.push(new Map());
+    } else users.push(await reaction.users.fetch());
+  }
+  const total = users.map((l) => l.size - 1).reduce((a, n) => a + n);
+  embed.fields = [];
+  for (const c of event.choices) {
+    const u = users[event.choices.indexOf(c)];
+    u.delete(client.user.id);
+    const name = `${c.emoji} ${c.name} (${u.size} - ${u.size == 0 ? "0" : Math.round((u.size * 1000) / total) / 10}%)`;
+    const value = `> ${
+      u.size == 0
+        ? "-"
+        : Array.from(u.values())
+            .map((u) => `<@${u.id}>`)
+            .join(", ")
+    }`;
+    embed.addField(name, value);
+  }
+  return await msg.edit({ embeds: [embed] });
 };
